ba6a4cf1bb12d7d98d839023e4f9f18688c4c2d9
rakeru2006 <rakeru2006@gmail.com>
2020-12-18 12:00:27 -0600

    adding routes to employees js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+//Iniciando el servidor con express 
 const express = require ("express");
 const app = express();
 
+//settings 
+app.set("port", process.env.PORT || 3000);
+
+// esta linea permite conocer la informacion dentro de json 
+app.use(express.json());
+
+// Routes  para procesar datos recibirlos etc 
+//app.use(require("./routes/employees"));
+
+// starting the server 
 app.listen(3000,() => {
-    console.log("server on port 3000");
+    console.log("server on port 3000", app.get("port"));
 }
 
 );
